1e94b282b32dca11ec1d7bc168573e521bf5c1e5
BadgerSnacks <Tanjer1588@gmail.com>
2026-03-25 17:06:11 -0700

    added install package function for importing missing packages.

--- main.py
+++ main.py
@@ -2,4 +2,17 @@
 By BadgerSnacks
 '''
 
+import subprocess
+import sys
+
+# Function to install missing packages.
+def install_package(package):
+    try:
+        __import__(package) # checks to see if the package can be import into the program
+    except ImportError: # runs if program cannot be imported
+        print(f"Installing {package}...") # printline to show the user what is being installed
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package]) # sends this command to the terminal to install with pip, will not work if user does not have pip
+
+install_package("ultralytics")
+
 import ultralytics
